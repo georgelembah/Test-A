3d25adfa86419cd6e1ae0e19eb84e231eaa91385
georgelembah <69888069+georgelembah@users.noreply.github.com>
2020-10-25 22:16:20 -0400

    Add files via upload

---
+++ Question1.py
@@ -0,0 +1,4 @@
+def hello_name(user_name = input("Enter Username")):
+    return user_name
+    print("hello" + hello_name() + "!")
+
---
+++ question3.py
@@ -0,0 +1,9 @@
+def max_num_in_list(a_list):
+    max = a_list{0}
+    for a in a_list
+    if a > max:
+        max = a
+    return max
+
+
+print(max_num_in_list({1, 2, 3, 4, 5, 6, 7, 8, 9, 10}))
